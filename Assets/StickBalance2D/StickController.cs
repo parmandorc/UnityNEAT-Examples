f26d513cd07d74bb809c159462724b33f48cf9a4
Pablo A. Rodríguez Codes <parmandorc@gmail.com>
2017-03-13 13:15:51 +0000

    StickBall2D - Experiment 1
    Inputs:
    - Vertical angle of the stick
    - Current velocity of the ball
    
    Output:
    - Velocity of the handle, limited to magnitude = 1.0f
    
    The ball is kept balanced, although the handle keeps on moving until it
    goes out of the screen.
    
    Next step:
    - It should keep the stick balanced inside the window

--- Assets/StickBalance2D/StickController.cs
+++ Assets/StickBalance2D/StickController.cs
@@ -6,32 +6,47 @@ using SharpNeat.Phenomes;
 public class StickController : UnitController {
 
 	private Rigidbody2D handle;
+	private Rigidbody2D ball;
 	private bool IsRunning;
 	private IBlackBox box;
+	private float fitness;
 
 	void Awake() {
 		handle = transform.Find ("Handle").GetComponent<Rigidbody2D> ();
+		ball = transform.Find ("Ball").GetComponent<Rigidbody2D> ();
 	}
 
 	void FixedUpdate () {
 
 		if (IsRunning) {
 
+			// Get inputs
+
+			Vector2 ballPosition = (ball.transform.position - handle.transform.position).normalized;
+			float ballAngle = ballPosition.x; // This is actually the cosine of the angle, but no need for the actual value
+
 			ISignalArray inputArr = box.InputSignalArray;
-			/*inputArr[0] = frontSensor;
-			inputArr[1] = leftFrontSensor;
-			inputArr[2] = leftSensor;
-			inputArr[3] = rightFrontSensor;
-			inputArr[4] = rightSensor;
-			*/
+			inputArr [0] = ballAngle;
+			inputArr [1] = ball.velocity.x;
+			inputArr [2] = ball.velocity.y;
+
+
+			// Run neural network
+
 			box.Activate();
+
+
+			// Process outputs
 
 			ISignalArray outputArr = box.OutputSignalArray;
 
-			float vx = (float)(outputArr[0] * 2 - 1) * 1000.0f;
-			float vy = (float)(outputArr[1] * 2 - 1) * 1000.0f;
+			float vx = (float)outputArr[0] * 2 - 1;
+			float vy = (float)outputArr[1] * 2 - 1;
 
-			handle.velocity = new Vector2(vx, vy);
+			handle.velocity = Vector2.ClampMagnitude(new Vector2(vx, vy), 1.0f);
+
+			// Update fitness
+			fitness += Mathf.Max(ballPosition.y * Time.deltaTime, 0.0f);
 		}
 	}
 
@@ -48,6 +63,6 @@ public class StickController : UnitController {
 
 	public override float GetFitness()
 	{
-		return 0;
+		return fitness;
 	}
 }
